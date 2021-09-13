3502dc642d63954a4f83916480f7b23d4aee1762
Nkechi Okeke <janecutie89@gmail.com>
2021-09-13 23:21:33 +0100

    create candidate entity in testr.domain

---
+++ Testr.Domain/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testr.Domain.Entities
+{
+    public class Users
+    {
+        [MaxLength(50)]
+        [EmailAddress]
+        [Required(ErrorMessage = "EmailAddress is Reqired")]
+        public string EmailAddress { get; set; }
+
+        [MaxLength(20)]
+        [Required(ErrorMessage = "Password is Reqired")]
+        public string Password { get; set; }
+
+        public DateTime LastLoginDate { get; set; }
+
+
+    }
+
+}
