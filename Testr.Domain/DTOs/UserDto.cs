3a643afa64b914d528d30ef4ea80b15914fad926
Nkechi Okeke <janecutie89@gmail.com>
2021-09-13 23:09:34 +0100

    create user and

---
+++ Testr.Domain/DTOs/UserDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testr.Domain.DTOs
+{
+    public class UserDto
+    {
+        public long UserId { get; set; }
+
+        [MaxLength(50)]
+        [EmailAddress]
+        [Required(ErrorMessage = "EmailAddress is Reqired")]
+        public string EmailAddress { get; set; }
+
+        public DateTime LastLoginDate { get; set; }
+    }
+}
