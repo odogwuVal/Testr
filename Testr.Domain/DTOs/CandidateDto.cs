18e1ba067de1c3154bb2396c2bc44027f6e22c23
Nkechi Okeke <janecutie89@gmail.com>
2021-09-14 23:45:12 +0100

    create candidatedto folder in domain

--- Testr.Domain/DTOs/CandidateDto.cs
+++ Testr.Domain/DTOs/CandidateDto.cs
@@ -11,62 +11,38 @@ namespace Testr.Domain.DTOs
     {
         public long CandidateId { get; set; }
 
-        [MaxLength(50)]
-        [Required(ErrorMessage = "FirstName is Reqired")]
         public string FirstName { get; set; }
-
-        [MaxLength(50)]
+                
         public string MiddleName { get; set; }
 
-        [MaxLength(50)]
-        [Required(ErrorMessage = "LastName is Reqired")]
         public string LastName { get; set; }
 
-        [MaxLength(50)]
-        [EmailAddress]
-        [Required(ErrorMessage = "EmailAddress is Reqired")]
         public string EmailAddress { get; set; }
 
-        [MaxLength(50)]
-        [Required(ErrorMessage = "Date of Birth is Reqired")]
         public DateTime DateOfBirth { get; set; }
-
-        [MaxLength(20)]
-        [Required(ErrorMessage = "Phone Number is Reqired")]
+                        
         public string PhoneNumber1 { get; set; }
 
-        [MaxLength(20)]
         public string PhoneNumber2 { get; set; }
 
-        [MaxLength(100)]
-        [Required]
         public string ResidentialAddress { get; set; }
 
-        [MaxLength(20)]
-        [Required]
         public string StateOfOrigin { get; set; }
 
-        [MaxLength(20)]
-        [Required]
         public string Gender { get; set; }
 
-        [MaxLength(50)]
-        [Required]
         public string CountryOfOrigin { get; set; }
 
         public string PhotoUrl { get; set; }
-
-        [Required]
+        
         public DateTime DateRegistered { get; set; }
 
         public DateTime LastModified { get; set; }
 
         public bool IsActive { get; set; }
 
-        [Required]
         public string AcademicQualification { get; set; }
 
-        [Required]
         public bool NYSCCompleted { get; set; }
 
         public string LinkedInUrl { get; set; }
