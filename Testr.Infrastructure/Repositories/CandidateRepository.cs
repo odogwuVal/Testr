72cf8418e7725a7ec0d52f1581ce514ee2bedaba
Nkechi Okeke <janecutie89@gmail.com>
2021-09-14 23:27:56 +0100

    create repository folder and implement the repository classes

---
+++ Testr.Infrastructure/Repositories/CandidateRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testr.Domain.Entities;
+using Testr.Domain.Interfaces;
+using Testr.Infrastructure.Authentication;
+
+namespace Testr.Infrastructure.Repositories
+{
+    public class CandidateRepository : Repository<Candidate>, ICandidateRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CandidateRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ Testr.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testr.Domain.Interfaces.Base;
+using Testr.Infrastructure.Authentication;
+
+namespace Testr.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly AppDbContext _appDbContext;
+        public Repository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            await _appDbContext.Set<T>().AddAsync(entity);
+            await _appDbContext.SaveChangesAsync();
+            return entity;
+        }
+        public async Task DeleteAsync(T entity)
+        {
+            _appDbContext.Set<T>().Remove(entity);
+            await _appDbContext.SaveChangesAsync();
+        }
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _appDbContext.Set<T>().ToListAsync();
+        }
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _appDbContext.Set<T>().FindAsync(id);
+        }
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
