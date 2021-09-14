98c0a0538212294164c98323d5223c184fce5f9c
Nkechi Okeke <janecutie89@gmail.com>
2021-09-14 23:33:45 +0100

    implement the repository classes in the interface folder in Domain

---
+++ Testr.Domain/Interfaces/ICandidateRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testr.Domain.Entities;
+using Testr.Domain.Interfaces.Base;
+
+namespace Testr.Domain.Interfaces
+{
+    public interface ICandidateRepository : IRepository<Candidate>
+    {
+        
+    }
+}
