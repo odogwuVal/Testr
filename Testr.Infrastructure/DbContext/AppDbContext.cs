d8b339351095b4d99ed2a0be4936a1fe5a8db1ac
Nkechi Okeke <janecutie89@gmail.com>
2021-09-15 00:02:27 +0100

    created DbContext folder inside infrastructure and moved appDbContext into it

---
+++ Testr.Infrastructure/DbContext/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Testr.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Testr.Infrastructure.Authentication
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, long>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>()
+                .Property("Id").UseIdentityColumn();
+
+            builder.Entity<ApplicationRole>()
+                .Property("Id").UseIdentityColumn();
+
+            builder.Entity<Candidate>()
+                .HasOne(c => c.User)
+                .WithOne();
+        }
+
+        public DbSet<Candidate> Candidates { get; set; }
+    }
+}
