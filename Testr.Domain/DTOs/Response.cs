0b796e9ca8d9dae83caef158224cde0ae512bc05
Nkechi Okeke <janecutie89@gmail.com>
2021-09-14 23:40:47 +0100

    implement Responses class in Domain

---
+++ Testr.Domain/DTOs/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testr.Domain.DTOs
+{
+    public class Response
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public object Payload { get; set; }
+    }
+}
