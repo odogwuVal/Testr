de45e9a14f8d3f032cf15381d29a32abbdc28c62
Bayowa Odometa <boyoanu@hotmail.com>
2021-09-17 21:40:54 +0100

    Implementation of crud operation to fetch all candidate and candidate by id on Test.API

--- Testr.API/Controllers/CandidatesController.cs
+++ Testr.API/Controllers/CandidatesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Testr.Domain.DTOs;
 using Testr.Domain.Entities;
@@ -15,10 +16,44 @@ namespace Testr.API.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ICandidateRepository _candidate;
 
+        Response responseBody = new Response();
         public CandidatesController(UserManager<ApplicationUser> userManager, ICandidateRepository candidate)
         {
             _userManager = userManager;
             _candidate = candidate;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Candidate>>> GetAllCandidate()
+        {
+            var candidates = await _candidate.GetAllAsync();
+           
+
+            var userCandidates = await _candidate.GetAllAsync();
+            if (userCandidates != null)
+            {
+                responseBody.Message = "Candidate Profile";
+                responseBody.Status = "Success";
+                responseBody.Payload = null;
+                return Ok(responseBody);
+            }
+
+            return Ok(candidates);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Candidate>> GetCandidateAsync([FromRoute] long id)
+        {
+            var candidate = await _candidate.GetByIdAsync(id);
+            if (candidate == null)
+            {
+                responseBody.Message = "Candidate with corresponding id does not exists";
+                responseBody.Status = "Failed";
+                responseBody.Payload = null;
+                return NotFound(responseBody);
+            }
+
+            return candidate;
         }
 
         [HttpPost]
