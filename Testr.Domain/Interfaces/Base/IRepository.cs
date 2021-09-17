a39de7a06f0af82a4dcdc232b12a1d5d527deeaa
Bayowa Odometa <boyoanu@hotmail.com>
2021-09-17 21:44:46 +0100

    Code refactor in IRepository.cs

--- Testr.Domain/Interfaces/Base/IRepository.cs
+++ Testr.Domain/Interfaces/Base/IRepository.cs
@@ -6,7 +6,7 @@ namespace Testr.Domain.Interfaces.Base
     public interface IRepository<T> where T : class
     {
         Task<IReadOnlyList<T>> GetAllAsync();
-        Task<T> GetByIdAsync(int id);
+        Task<T> GetByIdAsync(long id);
         Task<T> AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task DeleteAsync(T entity);
