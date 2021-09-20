152fc6d8360f710ae521c34ea6756257b919c8ca
Adeleke Ayinde <lekea4@gmail.com>
2021-09-20 16:11:07 +0100

    Implement the base class for Administrator entity

---
+++ Testr.Domain/Base/AdministratorBase.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Testr.Domain.Base
+{
+    public class AdministratorBase
+    {
+        [Required(ErrorMessage = "First Name is required."), MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required."), MaxLength(50)]
+        public string LastName { get; set; }
+
+
+        [Required(ErrorMessage = "Email Address is required."), MaxLength(150)]
+        [EmailAddress(ErrorMessage = "This is not a valid email address.")]
+        public string EmailAddress { get; set; }
+
+    }
+}
