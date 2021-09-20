e1aa5e6a45ebf951ddf1060a30c7b5e243edb36e
Bayowa Odometa <boyoanu@hotmail.com>
2021-09-20 16:46:54 +0100

    Refactor code in candidate controller

--- Testr.API/Controllers/CandidatesController.cs
+++ Testr.API/Controllers/CandidatesController.cs
@@ -14,9 +14,7 @@ namespace Testr.API.Controllers
     public class CandidatesController : ControllerBase
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        private readonly ICandidateRepository _candidateRepo;
-
-        Response responseBody = new Response();
+        private readonly ICandidateRepository _candidateRepo; 
         public CandidatesController(UserManager<ApplicationUser> userManager, ICandidateRepository candidate)
         {
             _userManager = userManager;
@@ -26,7 +24,9 @@ namespace Testr.API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Candidate>>> GetAllCandidate()
         {
+            Response responseBody = new Response();
             var candidates = await _candidateRepo.GetAllAsync();
+           // Response body when fetched
             if (candidates != null)
             {
                 responseBody.Message = "Sucessfully fetched all candidates";
@@ -34,16 +34,21 @@ namespace Testr.API.Controllers
                 responseBody.Payload = candidates;
                 return Ok(responseBody);
             }
-            // Set response body when fetched
-
-            responseBody.Message = "Successfully return responsebody";
+            
+            // Set response body when not fetched
+            responseBody.Message = "Candidates fetch failed";
+            responseBody.Status = "Failed";
+            responseBody.Payload = null;
             return Ok(responseBody);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Candidate>> GetCandidateAsync([FromRoute] long id)
         {
+            Response responseBody = new Response();
             var candidate = await _candidateRepo.GetByIdAsync(id);
+
+            // Reponse body when not found
             if (candidate == null)
             {
                 responseBody.Message = "Candidate with corresponding id does not exists";
@@ -53,8 +58,10 @@ namespace Testr.API.Controllers
             }
 
             // Set response body when found
-            responseBody.Message = "Sucessfully return responsebody";
-           
+            responseBody.Message = "Sucessfully fetched candidate with id";
+            responseBody.Status = "Success";
+            responseBody.Payload = candidate;
+
             return Ok(responseBody);
         }
 
