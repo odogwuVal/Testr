f92b767ff527d70cc72e7aedbcec232ad608ae5c
Adeleke Ayinde <lekea4@gmail.com>
2021-09-20 16:27:33 +0100

    Implement the Admin profile class

---
+++ Testr.Domain/DTOs/AdminProfileDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testr.Domain.Base;
+
+namespace Testr.Domain.DTOs
+{
+    public class AdminProfileDTO :AdministratorBase 
+    {
+        public long AdminId { get; set; }
+    }
+}
