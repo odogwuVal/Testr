11f07ce3f58b66d8e37e381bf0fa6fb89a4ba202
Adeleke Ayinde <lekea4@gmail.com>
2021-09-20 16:13:15 +0100

    Implement the Administrator enitity class

---
+++ Testr.Domain/Entities/Administrator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Testr.Domain.Base;
+
+namespace Testr.Domain.Entities
+{
+    public class Administrator : AdministratorBase
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        public DateTime LastModified { get; set; }
+
+        [Required]
+        public long UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+    }
+}
