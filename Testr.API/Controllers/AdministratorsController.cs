1dc9c20f9ed18c3cb8e2969da2366bfa2b3a6233
Philip Newman <philipeano@gmail.com>
2021-09-24 13:47:31 +0100

    Remove unnecessary line breaks

--- Testr.API/Controllers/AdministratorsController.cs
+++ Testr.API/Controllers/AdministratorsController.cs
@@ -17,82 +17,65 @@ namespace Testr.API.Controllers
     [ApiController]
     public class AdministratorsController : ControllerBase
     {
-        
-     
-        
-            private readonly UserManager<ApplicationUser> _userManager;
-            private readonly RoleManager<ApplicationRole> _roleManager;
-            private readonly IConfiguration _configuration;
-            private readonly IAdminRepository _adminRepository;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        private readonly IConfiguration _configuration;
+        private readonly IAdminRepository _adminRepository;
 
-            public AdministratorsController(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager,
-                    IConfiguration configuration, IAdminRepository adminRepository)
+        public AdministratorsController(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager,
+                IConfiguration configuration, IAdminRepository adminRepository)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+            _adminRepository = adminRepository;
+        }
+
+
+        [HttpPost]
+        [Route("register-admin")]
+        public async Task<IActionResult> RegisterAdmin([FromBody] AdminRegistrationDTO model)
+        {
+            Response responseBody = new Response();
+
+            ApplicationUser adminExist = await _userManager.FindByEmailAsync(model.EmailAddress);
+            if (adminExist != null)
             {
-                _userManager = userManager;
-                _roleManager = roleManager;
-                _configuration = configuration;
-                _adminRepository = adminRepository;
+                responseBody.Message = "An Administrator with this Email Address already exist";
+                responseBody.Status = "Failed";
+                responseBody.Payload = null;
+                return Conflict(responseBody);
             }
 
-
-            [HttpPost]
-            [Route("register-admin")]
-            public async Task<IActionResult> RegisterAdmin([FromBody] AdminRegistrationDTO model)
+            ApplicationUser user = new ApplicationUser()
             {
-                Response responseBody = new Response();
+                Email = model.EmailAddress,
+                UserName = model.EmailAddress,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
 
-                ApplicationUser adminExist = await _userManager.FindByEmailAsync(model.EmailAddress);
-                if (adminExist != null)
-                {
-                    responseBody.Message = "An Administrator with this Email Address already exist";
-                    responseBody.Status = "Failed";
-                    responseBody.Payload = null;
-                    return Conflict(responseBody);
-                }
-
-
-
-                ApplicationUser user = new ApplicationUser()
-                {
-                    Email = model.EmailAddress,
-                    UserName = model.EmailAddress,
-                    SecurityStamp = Guid.NewGuid().ToString()
-                };
-
-                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
-                if (!result.Succeeded)
-                {
-                    responseBody.Message = "Administrator registration was not successful. Please try again.";
-                    responseBody.Status = "Failed";
-                    responseBody.Payload = null;
-                    return BadRequest(responseBody);
-                }
-
-
-
-                 await _adminRepository.AddAsync(model, user);
-
-                if (!await _roleManager.RoleExistsAsync("Admin"))
-                    await _roleManager.CreateAsync(new ApplicationRole() { Name = "Admin" });
-
-
-                if (await _roleManager.RoleExistsAsync("Admin"))
-                    await _userManager.AddToRoleAsync(user, "Admin");
-
-                responseBody.Message = "Admin registration completed successfully.";
-                responseBody.Status = "Success";
-                responseBody.Payload = $" An Admin with email {user.UserName} has been provisioned" ;
-                return Created($"/users/{user.Id}", responseBody);
-
+            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+            {
+                responseBody.Message = "Administrator registration was not successful. Please try again.";
+                responseBody.Status = "Failed";
+                responseBody.Payload = null;
+                return BadRequest(responseBody);
             }
 
+            await _adminRepository.AddAsync(model, user);
+
+            if (!await _roleManager.RoleExistsAsync("Admin"))
+                await _roleManager.CreateAsync(new ApplicationRole() { Name = "Admin" });
 
 
+            if (await _roleManager.RoleExistsAsync("Admin"))
+                await _userManager.AddToRoleAsync(user, "Admin");
 
-
-
-
-
-        
+            responseBody.Message = "Admin registration completed successfully.";
+            responseBody.Status = "Success";
+            responseBody.Payload = $" An Admin with email {user.UserName} has been provisioned";
+            return Created($"/users/{user.Id}", responseBody);
+        }
     }
 }
