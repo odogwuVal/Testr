375529f44d943c54dda3769ca358ca824b221b18
Philip Newman <philipeano@gmail.com>
2021-09-24 13:39:10 +0100

    Define IAuthorizationHelper interface

---
+++ Testr.Application/Helpers/IAuthorizationHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testr.Application.Helpers
+{
+    public interface IAuthorizationHelper
+    {
+
+        public long GetCurrentCandidateId();
+
+
+        public Task<bool> CurrentUserHasRoleAsync(string roleName);
+    }
+}
