c66bc5a010fb500cdac3f10408d6c7018054f92f
Nkechi Okeke <janecutie89@gmail.com>
2021-10-02 00:49:09 +0100

    Included BulkEmail endpoint in EmailController

--- Testr.API/Controllers/EmailController.cs
+++ Testr.API/Controllers/EmailController.cs
@@ -1,46 +1,71 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Mail;
 using System.Threading.Tasks;
+using Testr.Domain.Interfaces;
+using Testr.Infrastructure.EmailModel;
+using Testr.Infrastructure.EmailServices;
 
 namespace Testr.API.Controllers
 {
     [Route("api/email")]
-    [ApiController]    
+    [ApiController]
+    //[Authorize (Roles = "SuperAdmin, Admin")]
     public class EmailController : ControllerBase
     {
-        [HttpPost]
-        [Route("send-email")]
-        public async Task SendEmail([FromBody] JObject objData)
+        private readonly IWebHostEnvironment _host;
+        private readonly IMailService _mailService;
+        private readonly ICandidateRepository _candidateRepo;
+
+
+        public EmailController(IWebHostEnvironment host, IMailService mailService, ICandidateRepository candidateRepo)
         {
-            var message = new MailMessage();
-            message.To.Add(new MailAddress(objData["toname"].ToString() + " <" + objData["toemail"].ToString() + ">"));
-            message.From = new MailAddress("Amit Mohanty <amitmohanty@email.com>");
-            message.Bcc.Add(new MailAddress("Amit Mohanty <amitmohanty@email.com>"));
-            message.Subject = objData["subject"].ToString();
-            message.Body = createEmailBody(objData["toname"].ToString(), objData["message"].ToString());
-            message.IsBodyHtml = true;
-            using (var smtp = new SmtpClient())
-            {
-                await smtp.SendMailAsync(message);
-                await Task.FromResult(0);
-            }
+            _host = host;
+            _mailService = mailService;
+            _candidateRepo = candidateRepo;
         }
 
-        private string createEmailBody(string userName, string message)
+
+        [HttpPost]
+        [Route("Single-email-send")]
+        public async Task SendEmail([FromBody] MailRequest mailRequest)
         {
-            string body = string.Empty;
-            using (StreamReader reader = new StreamReader(HttpContext.Current.Server.MapPath("/htmlTemplate.html")))
-            {
-                body = reader.ReadToEnd();
-            }
-            body = body.Replace("{UserName}", userName);
-            body = body.Replace("{message}", message);
-            return body;
+            await _mailService.SendEmailAsync(mailRequest);
+            await Task.FromResult(0);
         }
+
+        [HttpPost]
+        [Route("Bulk-email-send")]
+        public async Task SendBulkEmail([FromBody] MailRequest mailRequest)
+        {
+            var candidates = await _candidateRepo.GetAllAsync();
+            foreach (var candidate in candidates)
+            {
+                mailRequest.ToEmail = candidate.EmailAddress;
+                await _mailService.SendEmailAsync(mailRequest);
+                await Task.FromResult(0);
+            }            
+        }
+
+        //private string CreateEmailBody(string userName, string message)
+        //{
+        //    string body = string.Empty;
+        //    // using (StreamReader reader = new StreamReader(HttpContext.Current.Server.MapPath("/htmlTemplate.html")))
+        //    using (StreamReader reader = new StreamReader(Path.Combine(_host.ContentRootPath, 
+        //        "/Testr.Infrastructure/EmailTemplates/RegistrationAcknowledgment.txt")))
+        //    {
+        //        body = reader.ReadToEnd();
+        //    }
+        //    body = body.Replace("{FirstName}", userName);
+        //    // body = body.Replace("{message}", message);
+        //    return body;
+        //}
+
     }
 }
