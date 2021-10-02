b7c0706f3d2d07cdcc3f61cd49ca0ce68e759488
Nkechi Okeke <janecutie89@gmail.com>
2021-10-02 01:00:35 +0100

    Include Bcc and Cc in Mailrequest

--- Testr.Infrastructure/EMailModel/MailRequest.cs
+++ Testr.Infrastructure/EMailModel/MailRequest.cs
@@ -10,8 +10,15 @@ namespace Testr.Infrastructure.EmailModel
     public class MailRequest
     {
         public string ToEmail { get; set; }
+
         public string Subject { get; set; }
+
         public string Body { get; set; }
+
+        public string CcEmail { get; set; }
+
+        public string BccEmail { get; set; }
+
         public List<IFormFile> Attachments { get; set; }
     }
 }
