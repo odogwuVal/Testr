eb91251c34e290b585d247e33062b553c7e6acea
Nkechi Okeke <janecutie89@gmail.com>
2021-10-02 01:09:24 +0100

    Fix bug to allow access to sendEmail endpoint

--- Testr.Infrastructure/MailSettings.cs
+++ Testr.Infrastructure/MailSettings.cs
@@ -9,9 +9,13 @@ namespace Testr.Infrastructure
     public class MailSettings
     {
         public string Mail { get; set; }
+
         public string DisplayName { get; set; }
+
         public string Password { get; set; }
+
         public string Host { get; set; }
+
         public int Port { get; set; }
     }
 }
