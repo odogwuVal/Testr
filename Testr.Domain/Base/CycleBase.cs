d1f0330d502bfaa107b3477179f063932c721524
TheBulbFellows <OpenR3poD@Y>
2021-10-04 14:42:35 +0100

    Implement cycle base class

---
+++ Testr.Domain/Base/CycleBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testr.Domain.Base
+{
+   public  class CycleBase
+    {
+       [Required]
+        public string CycleName { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Status { get; set; }
+        [Required]
+        public DateTime DateOpened { get; set; }
+        [Required]
+        public DateTime DateClosed { get; set; }
+        [Required]
+        public DateTime LastModified { get; set; }
+    }
+}
