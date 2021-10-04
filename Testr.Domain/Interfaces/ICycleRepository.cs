0554754408f83bb28b9933e6acb977a4f13124b4
TheBulbFellows <OpenR3poD@Y>
2021-10-04 14:45:23 +0100

    Implements Interface cycle repository class

---
+++ Testr.Domain/Interfaces/ICycleRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testr.Domain.DTOs;
+using Testr.Domain.Entities;
+using Testr.Domain.Interfaces.Base;
+
+namespace Testr.Domain.Interfaces
+{
+    public interface ICycleRepository :IRepository<Cycle>
+    {
+        public Task AddAsync(CycleRegistrationDTO cycleInfo, ApplicationUser userInfo);
+        Task<object> DeleteAsync();
+        Task UpdateAsync(long id);
+    }
+}
