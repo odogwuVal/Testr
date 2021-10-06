a035f8cc1fd819fcf89337ea17edbd68558c2a10
Adeleke Ayinde <lekea4@gmail.com>
2021-10-06 12:47:41 +0100

    Modify the Account controller class to define the payload

--- Testr.API/Controllers/AccountController.cs
+++ Testr.API/Controllers/AccountController.cs
@@ -8,7 +8,7 @@ using Testr.Infrastructure.EmailServices;
 
 namespace Testr.API.Controllers
 {
-    [Authorize]
+    
     [Route("api/[controller]")]
     [ApiController]
     public class AccountController : ControllerBase
@@ -24,30 +24,30 @@ namespace Testr.API.Controllers
             _configuration = configuration;
         }
 
-        [Authorize(Roles = "Candidate")]
+        
         [HttpPost("ForgotPassword")]
         public async Task<IActionResult> ForgotPassword(string email)
         {
             Response responseBody = new Response();
             if (string.IsNullOrEmpty(email))
             {
-                responseBody.Message = $"Email address not specified";
+                responseBody.Message = "Email address not specified";
                 responseBody.Status = "Failed";
                 responseBody.Payload = null;
 
                 return BadRequest(responseBody);
             }
 
-            await _acccountService.ForgotPasswordAsync(email);
+            bool result = await _acccountService.ForgotPasswordAsync(email);
 
             responseBody.Message = $"A password reset link has been sent to {email}";
             responseBody.Status = "Success";
-            responseBody.Payload = null;
+            responseBody.Payload = result;
 
             return Ok(responseBody);
         }
 
-        [Authorize(Roles = "Candidate")]
+      
         [HttpPost("ResetPassword")]
         public async Task<IActionResult> ResetPassword([FromForm] ResetPasswordDTO model)
         {
@@ -61,11 +61,11 @@ namespace Testr.API.Controllers
                 return BadRequest(responseBody);
             }
 
-            await _acccountService.ResetPasswordAsync(model);
+            bool result = await _acccountService.ResetPasswordAsync(model);
 
             responseBody.Message = $"Password has been reset successfully";
             responseBody.Status = "Success";
-            responseBody.Payload = null;
+            responseBody.Payload = result ;
 
             return Ok(responseBody);
 
