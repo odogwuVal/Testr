059d63a864406777bf64d9a4cebf3f4cb43fe69d
Adeleke Ayinde <lekea4@gmail.com>
2021-10-06 11:11:19 +0100

    Implement the Account service interface for actions in the user account such as 'forgot password' and 'password reset'

---
+++ Testr.Infrastructure/AccountServices/IAccountService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testr.Domain.DTOs;
+using Testr.Domain.Entities;
+
+namespace Testr.Infrastructure.AccountServices
+{
+    public interface IAccountService
+    {
+        Task<bool> ForgotPasswordAsync(string email);
+        Task<bool> ResetPasswordAsync(ResetPasswordDTO model);
+    }
+}
