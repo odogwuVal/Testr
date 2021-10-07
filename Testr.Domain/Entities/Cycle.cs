9869138b48b3fac7d9731b221680f3a12af5cce7
TheBulbFellows <OpenR3poD@Y>
2021-10-07 11:08:11 +0100

    Refactors Code for entity cycle class

---
+++ Testr.Domain/Entities/Cycle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testr.Domain.Base;
+
+namespace Testr.Domain.Entities
+{
+    public class Cycle: CycleBase
+    {
+        [Key]
+        [Required]
+        public long CycleId { get; set; }
+
+        
+        [Required]
+        public long CreatedById { get; set; }
+
+        
+        [Required]
+        public Administrator CreatedBy { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+
+    }
+}
+
