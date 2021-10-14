e412f3a1d9eeff0536fad461c10d2da07f5db0e6
Madu Valentine <maduvalentine8@gmail.com>
2021-10-14 16:19:11 +0100

    Update Program.cs

--- Testr.API/Program.cs
+++ Testr.API/Program.cs
@@ -15,6 +15,7 @@ namespace Testr.API
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>(); 
+                    webBuilder.UseUrls("http://*:5003");
                 });
     }
 }
