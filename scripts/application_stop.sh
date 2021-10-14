874ff70147612489bcc26367a45be310266ff874
Madu Valentine <maduvalentine8@gmail.com>
2021-10-14 17:02:42 +0100

    Update application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -2,4 +2,13 @@
 
 echo "stopping Testr.API application"
 cd /var/www/Testr/
-killall dotnet || echo "Process was not running."
+echo " Enter the process name:"
+proc_name=dotnet
+if pgrep $proc_name
+then
+echo " $proc_name running "
+pkill $proc_name
+echo "$proc_name  got killed"
+else
+echo " $proc_name is not running/stopped "
+fi
