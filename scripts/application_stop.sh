bb79b522c72d2b8996b5270eda93aa41ab0fd83b
Madu Valentine <maduvalentine8@gmail.com>
2021-10-14 17:06:35 +0100

    Update application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -3,9 +3,4 @@
 echo "stopping Testr.API application"
 cd /var/www/Testr/
 echo " Enter the process name:"
-if pgrep dotnet
-then
-pkill dotnet
-else
-echo " dotnet is not running/stopped "
-fi
+pkill -9 -f dotnet || true
