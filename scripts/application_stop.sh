a2dcbc15d341b8d4ebc781580243aa02862a1e6c
Madu Valentine <maduvalentine8@gmail.com>
2021-10-15 09:58:22 +0100

    Update application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -2,5 +2,12 @@
 
 echo "stopping Testr.API application"
 cd /var/www/Testr/
-DOTNET="pgrep dotnet"
-pkill -9 dotnet
+#DOTNET="pgrep dotnet"
+ pgrep dotnet
+ t=$((echo $?) 2>&1)
+ if [ "$t" = "0" ]; then
+    sudo pkill -9 dotnet
+ else
+          exit
+ fi
+
