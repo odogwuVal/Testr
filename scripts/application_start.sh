b409431286bc1ab1f858113950429d7848451ec6
Madu Valentine <maduvalentine8@gmail.com>
2021-12-05 05:25:44 +0100

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
- cd /root/Testr
+ cd /home/ubuntu/Testr
  export ASPNETCORE_URLS="http://*:5000"
- dotnet ef database update -c AppDbContext -s Testr.API/Testr.API.csproj -p Testr.Infrastructure/Testr.Infrastructure.csproj
- dotnet publish -c Release -r ubuntu.16.04-x64 -o /var/www/Testr/
- cd /var/www/Testr
- nohup dotnet Testr.API.dll > /dev/null 2>&1&
+ dotnet run
